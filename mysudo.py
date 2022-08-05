bdee810e21555c17e916592880f1095b36567e82
Laurent Xu <7594962+laurent-xu@users.noreply.github.com>
2022-08-06 00:24:09 +0200

    Remove mysudo debug print

--- mysudo.py
+++ mysudo.py
@@ -25,5 +25,4 @@ if sudo_password_hash != hashlib.md5(sudo_password).hexdigest():
 # call sys.argv[1], and pass the arguments to the program
 # (you need to pass in argv[1] in the argument list
 #  as well, it is not prepended)
-print(sys.argv[1], "then", sys.argv[1:])
 os.execvp(sys.argv[1], sys.argv[1:])
